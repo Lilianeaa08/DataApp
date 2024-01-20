59c75042fe4f5bbcb6082e5b95885feafe083352
Lilianeaa08 <82713559+Lilianeaa08@users.noreply.github.com>
2024-01-20 20:47:54 -0300

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -8,7 +8,7 @@ import plotly.express as px
 st.set_page_config(layout= 'wide')
 st.title('Análise de Reclamações')
 
-dados = pd.read_csv('https://raw.githubusercontent.com/Lilianeaa08/datasets_testes/main/dados.csv', usecols=lambda column: column not in ['Unnamed: 0'])
+dados = pd.read_csv('https://raw.githubusercontent.com/Lilianeaa08/DataApp/main/dados.csv', usecols=lambda column: column not in ['Unnamed: 0'])
 
 # Aplicar tratamento categórico
 condicoes = [
